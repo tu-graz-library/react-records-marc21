d61dc5f4abe256ae26baf30364c059c4fefeba2b
Philipp Gualdi <philipp.gualdi@tugraz.at>
2023-05-30 15:30:41 +0200

    modification: show validation error messages

--- src/lib/components/MetadataFields.js
+++ src/lib/components/MetadataFields.js
@@ -1,12 +1,14 @@
 // This file is part of Invenio.
 //
-// Copyright (C) 2021-2022 Graz University of Technology.
+// Copyright (C) 2021-2023 Graz University of Technology.
 //
 // React-Records-Marc21 is free software; you can redistribute it and/or
 // modify it under the terms of the MIT License; see LICENSE file for more
 // details.
 
 import React, { Component } from "react";
+import _get from "lodash/get";
+import { getIn } from "formik";
 import PropTypes from "prop-types";
 import { Button, Form, Icon } from "semantic-ui-react";
 import { ArrayField, GroupField } from "react-invenio-forms";
@@ -16,6 +18,14 @@ import { LeaderField } from "./LeaderField";
 export class MetadataFields extends Component {
   render() {
     const { fieldPath } = this.props;
+    let form = "";
+    const getFieldErrors = (form, fieldPath) => {
+      return (
+        getIn(form.errors, fieldPath, null) ||
+        getIn(form.initialErrors, fieldPath, null)
+      );
+    };
+    const fieldsError = getFieldErrors(form, `${fieldPath}.fields`);
     return (
       <>
         <GroupField fieldPath={`${fieldPath}.leader`} className={"leader"}>
@@ -31,7 +41,7 @@ export class MetadataFields extends Component {
             const fieldPathPrefix = `${fieldPath}.fields.${indexPath}`;
             return (
               <GroupField optimized>
-                <MetadataField fieldPath={fieldPathPrefix} />
+                <MetadataField fieldPath={fieldPathPrefix} error={fieldsError}  />
                 <Form.Field width={1}>
                   <Button icon onClick={() => arrayHelpers.remove(indexPath)}>
                     <Icon name="close" />
@@ -47,6 +57,7 @@ export class MetadataFields extends Component {
 }
 
 MetadataFields.propTypes = {
+  form: PropTypes.object.isRequired,
   fieldPath: PropTypes.string,
 };
 
